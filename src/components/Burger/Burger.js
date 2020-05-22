bc9515bb9d2798399a5d2dddb208d8255dd98c4e
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-05-22 04:19:08 +0200

    Mostrar los ingredientes de manear dinamica

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,14 +2,18 @@ import React from 'react'
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-
 const Burger = (props) => {
+  const ingredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey}/>
+      })
+    })
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/> 
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="bacon"/>
-      <BurgerIngredient type="meat"/>
+      {ingredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   )
